3d2ab34b5d2d12730890cf66a535ac09bffd1daa
4ooc <22727824+4ooc@users.noreply.github.com>
2024-06-24 03:51:15 +0800

    feat: manage dotfiles with chezmoi

---
+++ home/private_dot_config/zsh/rc.d/50-hack.zsh
@@ -0,0 +1,42 @@
+sdkman_auto_env () {
+  if [[ -n $SDKMAN_ENV ]] && [[ ! $PWD =~ ^$SDKMAN_ENV ]] then
+    if [[ -e "$SDKMAN_ENV/.sdkmanrc" ]] then
+      sdk env clear
+    else
+      __sdkman_env_restore_default_version java
+    fi
+    unset SDKMAN_ENV
+  fi
+
+  if [[ -n $SDKMAN_ENV ]] && [[ $PWD =~ ^$SDKMAN_ENV ]] then
+    return
+  fi
+
+  if [[ -f .sdkmanrc ]]; then
+    sdk env
+  fi
+
+  if [[ ! $PWD =~ ^$CODE_SPACE/ ]]; then
+    return
+  fi
+
+  local workspace="$CODE_SPACE/${${PWD#$CODE_SPACE/}%%/*}" java_version
+  if [[ -f "$workspace/.sdkmanrc" ]]; then
+    if [[ $workspace != $PWD ]]; then
+      java_version=$(cat "$workspace/.sdkmanrc" | grep ^java= | awk -F= '{print $2}')
+      sdk use java $java_version
+      SDKMAN_ENV=$workspace
+    fi
+  elif [[ -f "$workspace/build.gradle" ]]; then
+    major_version=$(cat "$workspace/build.gradle" | grep 'sourceCompatibility' | grep -Eo '[0-9.]+' | sed s/^1\\.//)
+    java_version=$(sdk list java | awk '{print $2}' | grep -E "^$major_version\.")
+    if [[ -n $java_version ]]; then
+      sdk use java $java_version
+    else
+      __sdkman_echo_red "No java $major_version installed"
+    fi
+    SDKMAN_ENV=$workspace
+  fi
+}
+
++autocomplete:recent-directories() {}
